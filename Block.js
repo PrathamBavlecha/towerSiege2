c1b4095c7e4f4c072a9eaf876cb29de13a800071
popquiz189 <75843049+PrathamBavlecha@users.noreply.github.com>
2021-01-25 18:04:02 +0530

    Update Block.js

--- Block.js
+++ Block.js
@@ -3,10 +3,16 @@ class Box extends BaseClass {
       super(x,y,30,30);
       this.color = color
     }
-    display(){
-      push ()
-      fill (this.color)
-      super.display()
-      pop ()
+   display(){
+    console.log(this.body.speed);
+    if(this.body.speed < 3){
+        fill (this.color)
+        super.display();
+    }else{
+      World.remove(world, this.body);
+      push();
+      this.visiblity = this.visiblity - 0.05;
+      pop();
     }
+  }
   };
